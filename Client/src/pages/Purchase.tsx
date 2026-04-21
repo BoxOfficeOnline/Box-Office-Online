9b2235de5bb1db74efd917ec78a834fe7337cdc7
vcalapino <159312665+vcalapino@users.noreply.github.com>
2026-04-21 04:28:50 +0000

    Still Prepared to Integrate

--- Client/src/pages/Purchase.tsx
+++ Client/src/pages/Purchase.tsx
@@ -3,11 +3,13 @@ import Barcode from 'react-barcode';
 export default function Purchase() {
     return(
         <>
-        <div>
-             Thank you for your purchase!
-        </div>
-        <div>
-            <Barcode value="1" displayValue={false} />
+        <div className="purchase">
+            <div>
+                Thank you for your purchase!
+            </div>
+            <div>
+                <Barcode value="1" displayValue={false} />
+            </div>
         </div>
         </>
     )
