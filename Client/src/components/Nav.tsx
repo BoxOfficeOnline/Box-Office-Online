ae4c92ce8b29b7f38c5426f3edd3f9d7604ff8dc
vcalapino <159312665+vcalapino@users.noreply.github.com>
2026-04-21 19:52:50 +0000

    Front-End files added

--- Client/src/components/Nav.tsx
+++ Client/src/components/Nav.tsx
@@ -1,9 +1,21 @@
 import { Link } from 'react-router-dom'
+import '../App.css'
+import kuLogo from '../assets/KULogo.svg';
+import logo from '../assets/BOOLogo.png';
 
 function Nav() {
     return (
-        <nav>
-            <Link to="/">Home</Link>
+        <nav className="navbar">
+            <img src={kuLogo} alt="KU Logo" className="navLogo"/>
+            <img src={logo} alt="BOO Logo" className="navLogo"/>
+            <div className="navLinks">
+                <Link to="/">
+                    Home
+                </Link>
+                <Link to="/scan">
+                    Scan Tickets
+                </Link>
+            </div>
         </nav>
     )
 }
